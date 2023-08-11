2921dada0bc9be407a4c2c127c04a38ced55c899
JulioCesar <jczanotto@yahoo.com.br>
2023-08-11 08:33:22 -0300

    changed engine and conversation

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 from app import routes
+
+# Configuração para lidar com caracteres especiais
+app.config['JSON_AS_ASCII'] = False
--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,28 @@ openai.api_key = app.config['OPENAI_API_KEY']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    response_message = ''
+    conversation = []
+
+    if len(conversation) == 0:
+        conversation.append("ElizaGPT: Olá! Eu sou a ElizaGPT, uma psicanalista virtual. Como posso ajudar você hoje?")
 
     if request.method == 'POST':
         user_input = request.form['user_input']
         if user_input:
-            response = openai.Completion.create(
-                engine="davinci",
-                prompt=user_input,
-                max_tokens=50,
-                temperature=0.8
-            )
-            response_message = response.choices[0].text.strip()
+            conversation.append(f"User: {user_input}")
+            response_message = generate_response("\n".join(conversation))
+            conversation.append(f"ElizaGPT: {response_message}")
+        else:
+            response_message = ''
 
-    return render_template('index.html', response_message=response_message)
+    return render_template('index.html', conversation=conversation)
+    
+def generate_response(conversation):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # Use o modelo de chat GPT-3.5 Turbo
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": conversation}
+        ]
+    )
+    return response.choices[0].message['content'].strip()
